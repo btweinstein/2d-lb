88eb9cfa7cd5d8956588247d991a1e25503ae093
bryan <btweinstein@gmail.com>
2015-12-03 17:15:29 -0500

    Finally figured out how to add the opencl file without installing editable. Very tricky.

--- setup.py
+++ setup.py
@@ -5,26 +5,26 @@ import numpy as np
 
 
 extensions = [
-    Extension("LB_D2Q9.cython_dimensionless",
-              sources=["src/LB_D2Q9/cython_dimensionless.pyx"],
+    Extension("LB_D2Q9.dimensionless.cython_dimensionless",
+              sources=["LB_D2Q9/dimensionless/cython_dimensionless.pyx"],
               include_dirs = [np.get_include()]),
 
-    Extension("LB_D2Q9_OLD.pipe_cython",
-              sources=["src/LB_D2Q9_OLD/pipe_cython.pyx"],
+    Extension("LB_D2Q9.OLD.pipe_cython",
+              sources=["LB_D2Q9/OLD/pipe_cython.pyx"],
               include_dirs = [np.get_include()])
 ]
 
 setup(
     name='2d-lb',
     version='0.01',
-    package_dir={'':'src'},
-    packages=['LB_D2Q9', 'LB_D2Q9_OLD'],
+    packages=['LB_D2Q9', 'LB_D2Q9.OLD', 'LB_D2Q9.dimensionless'],
     include_package_data=True,
+    package_data={'':["*.cl"]},
     url='',
     license='',
     author='Bryan Weinstein, Matheus C. Fernandes',
     author_email='bweinstein@seas.harvard.edu',
     description='',
-    ext_modules = cythonize(extensions, annotate=True, reload_support=True),
+    ext_modules = cythonize(extensions, annotate=True),
     include_dirs = [np.get_include()]
 )
